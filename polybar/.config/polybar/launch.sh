9b1bab5ae8b56f69231d485a8a460ad4ef99d017
Enrique Mendoza <luis.e.1998@hotmail.com>
2022-12-08 08:36:56 -0700

    hypr config update

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,17 +10,22 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-desktop=$(echo $DESKTOP_SESSION)
+# desktop=$(echo $DESKTOP_SESSION)
 
-case $desktop in
-    hypr|/usr/share/xsessions/hypr)
-      # Dual screen is not possible in Hypr
-      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar primary 2>&1 | tee -a /tmp/polybar1.log &
-        # MONITOR=$m polybar --reload primary -c ~/.config/polybar/primary.config &
-        # MONITOR=$m polybar --reload secondary -c ~/.config/polybar/secondary.config &
-      done
-esac
+# case $desktop in
+#     hypr|/usr/share/xsessions/hypr)
+#       # Dual screen is not possible in Hypr
+#       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#         # MONITOR=$m polybar primary 2>&1 | tee -a /tmp/polybar1.log &
+#         MONITOR=$m polybar --reload primary & # -c ~/.config/polybar/primary.config
+#         MONITOR=$m polybar --reload secondary & # -c ~/.config/polybar/secondary.config
+#       done
+# esac
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload primary & 
+    MONITOR=$m polybar --reload secondary & 
+done
 
 echo "Bars launched..."
 
