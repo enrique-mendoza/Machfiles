ae29e11e5189660db1c546a873b32aa61b9d1ce5
Enrique Mendoza <luis.mendoza@infotec.mx>
2024-07-27 22:53:10 -0600

    update: nvim part 1

---
+++ nvim/.config/nvim/lua/user/plugins/mason.lua
@@ -0,0 +1,36 @@
+local M = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+}
+
+M.execs = {
+  "astro",
+  "bashls",
+  "cssls",
+  "html",
+  "jsonls",
+  "lua_ls",
+  "pyright",
+  "tsserver",
+}
+
+function M.config()
+  local wk = require "which-key"
+  wk.add {
+    { "<leader>lI", "<cmd>Mason<cr>", desc = "Mason Info" },
+  }
+
+  require("mason").setup {
+    ui = {
+      border = "rounded",
+    },
+  }
+  require("mason-lspconfig").setup {
+    ensure_installed = M.execs,
+  }
+end
+
+return M
