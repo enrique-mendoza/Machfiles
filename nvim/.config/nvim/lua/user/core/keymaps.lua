65313077803aaa1c6d7cc2d7422cc8469bc19ad7
Enrique Mendoza <luis.mendoza@infotec.mx>
2024-08-16 09:07:00 -0600

    feat: resize keymaps nvim

--- nvim/.config/nvim/lua/user/core/keymaps.lua
+++ nvim/.config/nvim/lua/user/core/keymaps.lua
@@ -10,6 +10,11 @@ keymap("n", "<m-k>", "<C-w>k", opts)
 keymap("n", "<m-l>", "<C-w>l", opts)
 keymap("n", "<m-tab>", "<c-6>", opts)
 
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
 keymap("n", "n", "nzz", opts)
 keymap("n", "N", "Nzz", opts)
 keymap("n", "*", "*zz", opts)
